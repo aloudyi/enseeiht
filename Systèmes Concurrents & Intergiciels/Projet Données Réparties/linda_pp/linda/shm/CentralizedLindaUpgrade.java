ad6d81cc1f9e4b32658b3ac67b3c6797520e8a3b
zenon <35837688+aloudyi@users.noreply.github.com>
2022-05-01 17:22:02 +0000

    fixed indexOfTemplate

--- Systèmes Concurrents & Intergiciels/Projet Données Réparties/linda_pp/linda/shm/CentralizedLindaUpgrade.java
+++ Systèmes Concurrents & Intergiciels/Projet Données Réparties/linda_pp/linda/shm/CentralizedLindaUpgrade.java
@@ -463,8 +463,8 @@ public class CentralizedLindaUpgrade implements Linda {
 							indexOfTemplate = result+offset0;
 							result = -1;
 						}
-						allThreadsFinished++; // On incrémente un compteur
 					}
+					allThreadsFinished++; // On incrémente un compteur
 				}
 			}.start();
 		}
